7e07401061fab790e3ae61f5e9cefd4dbec8057b
bwesterg <benton.westergaard@gmail.com>
2021-07-20 14:16:44 -0600

    Added basic form, data is in state, some general styling.  Need to create a save point.

--- src/components/AirlineItem.js
+++ src/components/AirlineItem.js
@@ -3,12 +3,11 @@ import React from 'react';
 export default function AirlineItem({name, image, review, rating, high_marks}) {
     return (
         <li className="airline-item">
-            <h2>{name}</h2>
-            <h3>{image}</h3>
-            <h4>{review}</h4>
-            <h4>{rating}</h4>
+            <h4>{name}</h4>
+            <img src={image} alt={name} />
+            <h5>Review: <i>"{review}"</i></h5>
+            <h5>Overall Rating:</h5><h4>{rating}/10</h4>
             <h4>{high_marks}</h4>
-
         </li>
     )
 }
